e493e454b9dd9e7ca62885325479bcdb24f88994
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 14:58:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, matcher){
-  drivers.filter(function(driver){ driver == matcher})
+  drivers.filter(function(driver){ driver.toUpperCase() == matcher.toUpperCase()})
 }
