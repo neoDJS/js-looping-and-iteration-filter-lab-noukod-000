a150cb47432aee0f331d75aa5cbf8a9bf68136c6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 15:24:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, matcher){
+  return drivers.filter(function(driver){ return driver.toUpperCase() == matcher.toUpperCase()});
+}
+
+function fuzzyMatch(drivers, matcher){
+  return drivers.filter(function(driver){ return driver.toUpperCase().startsWith(matcher.toUpperCase())});
+}
+
+function matchName(drivers, matcher){
+  return drivers.filter(function(driver){ return driver.name.toUpperCase().match(matcher.toUpperCase())});
+}
